82a99916589aed4629cca4ee6549d37ed30ec291
Kshitij Limaye <klimaye@gmail.com>
2014-07-24 11:50:42 -0500

    play with stack layout and scroll view

--- XFormsPlay/App.cs
+++ XFormsPlay/App.cs
@@ -7,14 +7,39 @@ namespace XFormsPlay
 	{
 		public static Page GetMainPage ()
 		{	
-			return new ContentPage { 
-				Content = new Label {
-					Text = "Hello, Forms!",
-					VerticalOptions = LayoutOptions.CenterAndExpand,
-					HorizontalOptions = LayoutOptions.CenterAndExpand,
-				},
-			};
+			return new MainPage ();
 		}
+
+		public class MainPage : ContentPage
+		{
+			public MainPage ()
+			{
+				Label theLabel = new Label() {
+					Text = " click ",
+					HorizontalOptions = LayoutOptions.CenterAndExpand,
+					VerticalOptions = LayoutOptions.CenterAndExpand
+				};
+
+				Button theButton = new Button() {
+					Text = "Click Me",
+					HorizontalOptions = LayoutOptions.CenterAndExpand,
+					VerticalOptions = LayoutOptions.CenterAndExpand
+				};
+				theButton.Clicked +=(sender, e) => {
+					theLabel.Text = string.Format("{0} {1}"," click ",theLabel.Text);
+				};
+
+				var container = new StackLayout() {
+					Padding = new Thickness(20, Device.OnPlatform(20,0,0),20,20)
+				};
+
+				container.Children.Add(theButton);
+				container.Children.Add(theLabel);
+				Content = new ScrollView() { Content = container };
+			}
+
+		}
+
 	}
 }
 
